756da022fe7d7de65f03262c2f8817e19d1d0b26
Ilkka Lehtinen <ilkka.lehtinen@opuscapita.com>
2018-02-17 07:54:51 +0200

    Fix fluid columns example

--- examples/components/fluid-columns.component.jsx
+++ examples/components/fluid-columns.component.jsx
@@ -9,14 +9,14 @@ function FluidColumnsContent() {
   return (
     <div className="fluid-columns-container">
       <FluidColumns>
-        <div className="content">content 1</div>
-        <div className="content">content 2</div>
-        <div className="content">content 3</div>
-        <div className="content">content 4</div>
-        <div className="content">content 5</div>
-        <div className="content">content 6</div>
-        <div className="content">content 7</div>
-        <div className="content">content 8</div>
+        <div id="1" className="content">content 1</div>
+        <div id="2" className="content">content 2</div>
+        <div id="3" className="content">content 3</div>
+        <div id="4" className="content">content 4</div>
+        <div id="5" className="content">content 5</div>
+        <div id="6" className="content">content 6</div>
+        <div id="7" className="content">content 7</div>
+        <div id="8" className="content">content 8</div>
       </FluidColumns>
     </div>
   );
--- src/fluid-columns/fluid-columns.component.jsx
+++ src/fluid-columns/fluid-columns.component.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { List } from 'immutable';
+
 import './fluid-columns.scss';
 
 const FluidColumns = props => (
